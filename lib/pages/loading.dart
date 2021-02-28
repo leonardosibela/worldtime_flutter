bd23f49d335ae883780c356fee4ee76daed7b988
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-28 16:14:14 -0300

    Printing london time using world time api

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'dart:convert';
 
 class Loading extends StatefulWidget {
 
@@ -10,17 +11,22 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getData() async {
-    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+  void getTime() async {
+    Response response = await get('http://worldtimeapi.org/api/timezone/Europe/London');
     Map data = jsonDecode(response.body);
-    print(data);
-    print(data['title']);
+
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
   }
 
   @override
   void initState() {
     super.initState();
-    getData();
+    getTime();
   }
 
   @override
