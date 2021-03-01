6acae983342a6fbdb7e630fd6dd13e349405edb2
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-01 10:44:16 -0300

    Improving home screen colors

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -37,8 +37,16 @@ class _HomeState extends State<Home> {
                     onPressed: () {
                       Navigator.pushNamed(context, '/location');
                     },
-                    icon: Icon(Icons.edit_location),
-                    label: Text('Edit location')
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.grey[300],
+                    ),
+                    label: Text(
+                      'Edit location',
+                      style: TextStyle(
+                        color: Colors.grey[300]
+                      ),
+                    )
                 ),
                 SizedBox(height: 20.0),
                 Row(
@@ -48,7 +56,8 @@ class _HomeState extends State<Home> {
                       data['location'],
                       style: TextStyle(
                         fontSize: 28.0,
-                        letterSpacing: 2.0
+                        letterSpacing: 2.0,
+                        color: Colors.white
                       ),
                     )
                   ],
@@ -58,6 +67,7 @@ class _HomeState extends State<Home> {
                   data['time'],
                   style: TextStyle(
                     fontSize: 66.0,
+                      color: Colors.white
                   ),
                 )
               ],
